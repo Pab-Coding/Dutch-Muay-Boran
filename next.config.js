6e692113afb0f6f3155e9dc60ca0692880e22f8d
Pab-Coding <pabantca@gmail.com>
2025-01-29 20:03:53 +0100

    Fix: Images not displaying

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'img.youtube.com',
-          port: '',
-          pathname: '/vi/**',
-        },
-      ],
-    },
-  }
-  
-  module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+    ],
+    unoptimized: false, // Asegura que Next.js optimice las imágenes
+    dangerouslyAllowSVG: true, // Si estás usando SVGs
+    contentDispositionType: 'attachment', // Ayuda con algunos tipos de archivos
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
+
+module.exports = nextConfig
