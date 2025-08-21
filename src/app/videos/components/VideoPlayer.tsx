bb28cfa9426b3ab79fa0d1b7a8e8fde5b0d8e91e
Pablo Guillén Cañete <pabloguillencanete@mac.home>
2025-08-21 12:42:18 +0200

    Fix YouTube video player controls visibility
    Add explicit controls=1 parameter to YouTube embed URL to ensure
    play/pause button and other controls are always visible on desktop

--- src/app/videos/components/VideoPlayer.tsx
+++ src/app/videos/components/VideoPlayer.tsx
@@ -168,7 +168,7 @@ const VideoPlayer = ({ videoId, isOpen, onClose, title, description }: VideoPlay
               {/* Use 56.25% aspect ratio plus a small extra space for controls rendering */}
               <div className="relative bg-black sm:rounded-t-2xl" style={{ paddingTop: '56.25%' }}>
                 <iframe
-                  src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&playsinline=1&modestbranding=1`}
+                  src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&playsinline=1&modestbranding=1&controls=1`}
                   title="YouTube video player"
                   allow="accelerometer; autoplay; clipboard-write; 
                          encrypted-media; gyroscope; picture-in-picture; fullscreen"
